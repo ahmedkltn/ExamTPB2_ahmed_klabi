bb14150dd59eef17a53d98ea555b483f2b19a3b3
Ahmed klabi <ahmetklabi10@gmail.com>
2025-07-09 19:49:23 +0100

    fixed merge

--- projet.py
+++ projet.py
@@ -1,4 +1,5 @@
 
+
 def produit(T):
     S=0
     for t in T:
@@ -6,6 +7,10 @@ def produit(T):
 	return S
 
 Data=[1,3,5]
-Prod=math.prod(Data)
-print('le produit est :', Prod)
-
+if Data:
+	print('le produit est :', Prod)
+	print('le max est', max(Data))
+	print('le min est', min(Data))
+else:
+	print('dossier vide')
+	projetb2
